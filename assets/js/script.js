ad634beebfe6962e1cf63494df92110b69184d0e
Luigi Raynel <devluigiraynel@gmail.com>
2020-12-22 11:57:59 -0300

    finish mvp

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,55 @@
+  
 const form = document.getElementById('form');
-form.addEventListener('submit', handleSubmit)
+form.addEventListener('submit', handleSubmit);
 
-function handleSubmit(event){
-        event.preventDefault();
+function handleSubmit(event) {
+  event.preventDefault();
 
-        const age = document.getElementById('age').value;
+  const gender = getSelectedValue('gender');
+  const age = getInputNumberValue('age');
+  const weight = getInputNumberValue('weight');
+  const height = getInputNumberValue('height');
+  const activityLevel = getSelectedValue('activity_level');
 
-        console.log(age);
+  const taxaMetabolicaBasal = Math.round(
+    gender === 'female'
+      ? (655 + (9.6 * weight) + (1.8 * height) - (4.7 * age))
+      : (66 + (13.7 * weight) + (5 * height) - (6.8 * age))
+  );
+  const maintenance = Math.round(taxaMetabolicaBasal * Number(activityLevel));
+  const loseWeight = maintenance - 450;
+  const gainWeight = maintenance + 450;
+
+  const layout = `
+    <h2>Aqui está o resultado:</h2>
+    <div class="result-content">
+      <ul>
+        <li>
+          Seu metabolismo basal é de <strong>${taxaMetabolicaBasal} calorias</strong>.
+        </li>
+        <li>
+          Para manter o seu peso você precisa consumir em média <strong>${maintenance} calorias</strong>.
+        </li>
+        <li>
+          Para perder peso você precisa consumir em média <strong>${loseWeight} calorias</strong>.
+        </li>
+        <li>
+          Para ganhar peso você precisa consumir em média <strong>${gainWeight} calorias</strong>.
+        </li>
+      </ul>
+    </div>
+  `;
+
+  const result = document.getElementById('result');
+
+  result.innerHTML = layout;
+}
+
+function getSelectedValue(id) {
+  const select = document.getElementById(id);
+  return select.options[select.selectedIndex].value;
+}
+
+function getInputNumberValue(id) {
+  return Number(document.getElementById(id).value);
 }
